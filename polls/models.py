9c91597816db5213a59cb1603e6fcdcbb68b53ff
AlexTaranukha <alex_taranukha@yahoo.com>
2025-03-22 13:53:22 -0500

    Updated models

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -11,6 +10,4 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
-```
 
